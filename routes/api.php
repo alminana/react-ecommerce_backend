101157b15094b4199c5aadf226d4b63aef4eaf6a
alminana <alminanaeilerbutad20@gmail.com>
2022-12-07 23:10:55 +0300

    api category

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
-
+use App\Http\Controllers\Admin\CategoryController;
 // get Visitor Route
 Route::get('/getvisitor',[VisitorController::class, 'GetVisitorDetails']);
 //Contact Route
 Route::post('/postcontact',[ContactController::class, 'PostContactDetails']);
 //Site Info Route
 Route::get('/allsiteinfo',[SiteInfoController::class, 'AllSiteinfo']);
+//All category Route
+Route::get('/allcategory',[CategoryController::class, 'AllCategory']);
